3a0bd1ef79e82e53898b14951bb959ea8a64d58e
Antonio <khaingjar@gmail.com>
2022-03-30 22:01:35 +0630

    server mistake change

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -15,8 +15,8 @@ const Chat = () => {
   const [message,setMessage] = useState('')
   const [messages,setMessages] = useState([]);
   const [users,setUsers] = useState(null);
-  //const SERVER = 'https://hk-simple-chat-app.herokuapp.com/'
-  const SERVER = 'http://localhost:5000'
+  const SERVER = 'https://hk-simple-chat-app.herokuapp.com/'
+ 
   
   useEffect(() =>{
     const {name,room} = queryString.parse(window.location.search);
